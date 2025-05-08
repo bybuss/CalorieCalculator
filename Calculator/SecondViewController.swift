20e6b8828dc8fceda8a5ca647ed31c79816a45d8
bybuss <kukuhaha729@gmail.com>
2025-05-08 11:35:03 -0700

    initial commit

---
+++ Calculator/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  Calculator
+//
+//  Created by Никита Светличный on 08.05.2025.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
